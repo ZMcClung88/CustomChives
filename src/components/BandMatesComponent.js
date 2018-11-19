5253e4c5662f4f5dc0a3ea8d7d70931e927636d7
Zac McClung <zac.mcclung370@gmail.com>
2018-11-18 20:47:33 -0500

    saving changes

--- src/components/BandMatesComponent.js
+++ src/components/BandMatesComponent.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const BandMatesComponent = props => {
   return (
-    <div style={{ height: 'auto', width: '100%', border: '1px solid blue', display: 'flex', justifyContent: 'center' }}>
+    <div style={{ height: 'auto', width: '90%', border: '1px solid blue' }}>
+      <div style={{ height: '75px', width: '75px', border: '2px solid orange' }}>
+        <p>name</p>
+      </div>
       <div style={{ height: '75px', width: '75px', border: '2px solid orange' }}>
         <p>name</p>
       </div>
