388bf05710ab139e558da480d1e417fac249bea8
Zac McClung <zac.mcclung370@gmail.com>
2018-11-30 20:32:55 -0500

    saving changes

--- src/components/SongComponent.js
+++ src/components/SongComponent.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
+import img from '../media/chives_logo.JPG';
+
 const SongComponent = props => {
   return (
     <div
       style={{ height: '150px', width: '100%', border: '1px solid blue', display: 'flex', justifyContent: 'center' }}
     >
-      <button style={{ height: '20%', width: '100px' }}>Play</button>
+      <div>
+        <img style={{ height: '50px', width: '50px' }} src={img} />
+      </div>
+      <div>
+        <button style={{ height: '20%', width: '100px' }}>Play</button>
+      </div>
     </div>
   );
 };
